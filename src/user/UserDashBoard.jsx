53994e3f4bf6ef96ec4a182220ced309899738a0
viettu2k <doanviettu2k@gmail.com>
2021-11-02 08:54:13 +0700

    updating user dashboard

--- src/user/UserDashBoard.jsx
+++ src/user/UserDashBoard.jsx
@@ -29,7 +29,14 @@ export default function Dashboard() {
       </div>
 
       <div className="card mb-5">
-        <h3 className="card-header">Purchase history</h3>
+        <h3 className="card-header">Vaccination history</h3>
+        <ul className="list-group">
+          <li className="list-group-item">history</li>
+        </ul>
+      </div>
+
+      <div className="card mb-5">
+        <h3 className="card-header">Test history</h3>
         <ul className="list-group">
           <li className="list-group-item">history</li>
         </ul>
