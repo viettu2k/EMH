6d945a1bcd60857d6753bcea03c6853e5f491e01
viettu2k <doanviettu2k@gmail.com>
2021-11-02 09:15:39 +0700

    adding links on user dashboard and reafactoring

--- src/user/UserDashBoard.jsx
+++ src/user/UserDashBoard.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
   const {
     user: { _id, name, email, role },
   } = isAuthenticated();
 
-  return (
-    <Layout
-      title="Dashboard"
-      description="User Dashboard"
-      className="container"
-    >
+  const userLinks = () => {
+    return (
+      <div className="card">
+        <h4 className="card-header">User Links</h4>
+        <ul className="list-group">
+          <li className="list-group-item">
+            <Link className="nav-link" to="/profile/update">
+              Update Profile
+            </Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  const userInfo = () => {
+    return (
       <div className="card mb-5">
         <h3 className="card-header">User Information</h3>
         <ul className="list-group">
@@ -27,19 +39,44 @@ export default function Dashboard() {
           </li>
         </ul>
       </div>
+    );
+  };
 
+  const vaccinationHistory = () => {
+    return (
       <div className="card mb-5">
         <h3 className="card-header">Vaccination history</h3>
         <ul className="list-group">
           <li className="list-group-item">history</li>
         </ul>
       </div>
+    );
+  };
 
+  const testHistory = () => {
+    return (
       <div className="card mb-5">
         <h3 className="card-header">Test history</h3>
         <ul className="list-group">
           <li className="list-group-item">history</li>
         </ul>
+      </div>
+    );
+  };
+
+  return (
+    <Layout
+      title="Dashboard"
+      description="User Dashboard"
+      className="container"
+    >
+      <div className="row">
+        <div className="col-3">{userLinks()}</div>
+        <div className="col-9">
+          {userInfo()}
+          {vaccinationHistory()}
+          {testHistory()}
+        </div>
       </div>
     </Layout>
   );
