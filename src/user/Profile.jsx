d085383cbf3ac82bfcba346e6b3ef881e7e844f3
viettu2k <doanviettu2k@gmail.com>
2021-11-07 19:48:57 +0700

    updating get user info

--- src/user/Profile.jsx
+++ src/user/Profile.jsx
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import { read, update, updateUser } from "./apiUser";
 
-export default function Profile() {
-  return <div>profile</div>;
+export default function Profile({ match }) {
+  const { token } = isAuthenticated();
+
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: false,
+    success: false,
+  });
+
+  const { name, email, password, error, success } = values;
+
+  const init = (userId) => {
+    read(userId, token).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: true });
+      } else {
+        setValues({
+          ...values,
+          name: data.name,
+          email: data.email,
+          password: data.password,
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    init(match.params.userId);
+  }, []);
+
+  return (
+    <Layout
+      title="Profile"
+      description="Update your profile"
+      className="container-fluid"
+    >
+      <h2 className="mb-4">Profile Update</h2>
+      {JSON.stringify(values)}
+    </Layout>
+  );
 }
