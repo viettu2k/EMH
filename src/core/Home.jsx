f0a0060bcb5577a8f5c41659490b65c060f7c6df
viettu2k <doanviettu2k@gmail.com>
2021-11-09 10:01:05 +0700

    adding get vaccination list

--- src/core/Home.jsx
+++ src/core/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "./Layout";
+import Vaccinations from "../vaccination/Vaccinations";
 
 export default function Home() {
   return (
     <Layout title="Home Page" description="ELECTRONIC MEDICAL HANDBOOK ">
-      ...
+      <Vaccinations />
     </Layout>
   );
 }
---
+++ src/vaccination/Vaccinations.jsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+import { getVaccinations } from "./apiVaccination";
+import { Link } from "react-router-dom";
+
+export default function Vaccinations() {
+  const [values, setValues] = useState({
+    vaccinations: [],
+    error: "",
+  });
+
+  const { vaccinations, error } = values;
+
+  const init = () => {
+    getVaccinations().then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error });
+      } else {
+        setValues({ ...values, vaccinations: data });
+      }
+    });
+  };
+
+  useEffect(
+    () => {
+      init();
+    },
+    // eslint-disable-next-line
+    []
+  );
+
+  const renderVaccinations = (vaccinations) => (
+    <table class="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Type</th>
+          <th scope="col">Address</th>
+          <th scope="col">Ownership</th>
+          <th scope="col">View</th>
+        </tr>
+      </thead>
+      <tbody>
+        {vaccinations &&
+          vaccinations.map((v, i) => (
+            <tr>
+              <th scope="row">{i}</th>
+              <td>{v.name}</td>
+              <td>{v.type}</td>
+              <td>{v.address}</td>
+              <td>{v.ownership}</td>
+              <td>
+                <Link
+                  to={`/vaccinations/${v._id}`}
+                  className="btn btn-raised btn-primary btn-sm"
+                >
+                  View Information
+                </Link>
+              </td>
+            </tr>
+          ))}
+      </tbody>
+    </table>
+  );
+
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  return (
+    <div className="container">
+      <h2 className="mt-5 mb-5">
+        {!vaccinations.length ? "No more vaccinations!" : "Recent Vaccinations"}
+      </h2>
+      {renderVaccinations(vaccinations)}
+      {showError()}
+    </div>
+  );
+}
