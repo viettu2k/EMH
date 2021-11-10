4167909cde3078fcb80152374ad2f44c6c640f9f
viettu2k <doanviettu2k@gmail.com>
2021-11-10 10:23:23 +0700

    refactoring code

--- src/core/Home.jsx
+++ src/core/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "./Layout";
-import Vaccinations from "../vaccination/Vaccinations";
+import Vaccinations from "./Vaccinations";
 
 export default function Home() {
   return (
--- src/core/Vaccinations.jsx
+++ src/core/Vaccinations.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getVaccinations, getCenter } from "./apiVaccination";
+import { getVaccinations, getCenter } from "./apiCore";
 import { Link } from "react-router-dom";
 
 export default function Vaccinations() {
